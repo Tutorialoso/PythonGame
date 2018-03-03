6e6311086f7a7bbeb300be9c25947bea68387978
Alex Izbrodin <sizbrodin@gmail.com>
2018-03-03 15:14:43 +0000

    Part of basic game logic
    - Changed grid so it doesn't cover the whole window
    - Dot-assigning method with small tolerance for clicking precision
    - Array describing status of clickable areas
        + Elements True when valid to be clicked, False when already clicked before
    - Dot colour changes after every valid move

--- DotDot Pygame.py
+++ DotDot Pygame.py
@@ -18,9 +18,14 @@ GREY  = (200, 200, 200)
 
 BACKGROUND_COLOUR = GREY
 DOT_COLOUR = BLACK
+PLAYER_ONE_COLOUR = BLUE
+PLAYER_TWO_COLOUR = RED
+
+PLAYER_ONE = "1"
+PLAYER_TWO = "2"
 
 def main():
-    global FPS_CLOCK, DISPLAYSURF
+    global FPS_CLOCK, DISPLAYSURF, grid_array
     pygame.init()
     FPS_CLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -28,17 +33,30 @@ def main():
 
     mouse_x = 0
     mouse_y = 0
-    firstSelection = None
+    mouseClicked = None
+
+    current_player = PLAYER_ONE
+    last_colour = PLAYER_ONE_COLOUR
 
     DISPLAYSURF.fill(BACKGROUND_COLOUR)
-    draw_grid()
+    grid_array = init_grid()
     
     while True:
-        mouseClicked = False
+        mouseCliked = False
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONUP:
                 mouse_x, mouse_y = event.pos
                 mouseClicked = True
+                grid_x, grid_y = pixel_to_grid(mouse_x, mouse_y)
+                if (grid_x != None) and (grid_y != None):
+                    if (grid_x > 0) and (grid_y > 0) and (grid_x < 16) and (grid_y < 12):
+                        valid = is_click_valid(grid_x, grid_y)
+                        if valid == True:
+                            draw_dot(grid_x, grid_y, current_player)
+                    if current_player == PLAYER_ONE:
+                        current_player = PLAYER_TWO
+                    else:
+                        current_player = PLAYER_ONE
             elif event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -46,11 +64,20 @@ def main():
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
         
-def draw_grid():
-    for x in range(0, WINDOW_WIDTH, GAP_SIZE):
-        pygame.draw.line(DISPLAYSURF, BLACK, (x, 0), (x, 480), 1)
-    for y in range(0, WINDOW_HEIGHT, GAP_SIZE):
-        pygame.draw.line(DISPLAYSURF, BLACK, (0, y), (640, y), 1)
+def init_grid():
+    grid_array = []
+    for i in range(0, (int(WINDOW_HEIGHT / GAP_SIZE) - 1)):
+        grid_sub_array = []
+        for j in range(0, (int(WINDOW_WIDTH / GAP_SIZE) - 1)):
+            grid_sub_array.append(True)
+        grid_array.append(grid_sub_array)
+    for l in range (0, len(grid_array)):
+        print(grid_array[l])
+    for x in range(GAP_SIZE, (WINDOW_WIDTH), GAP_SIZE):
+        pygame.draw.line(DISPLAYSURF, BLACK, (x, GAP_SIZE), (x, (WINDOW_HEIGHT - GAP_SIZE)), 1)
+    for y in range(GAP_SIZE, (WINDOW_HEIGHT), GAP_SIZE):
+        pygame.draw.line(DISPLAYSURF, BLACK, (GAP_SIZE, y), ((WINDOW_WIDTH - GAP_SIZE), y), 1)
+    return grid_array
 
 def pixel_to_grid(mouse_x, mouse_y):
     if ((mouse_x % GAP_SIZE) > (TOLERANCE) + 10):
@@ -65,7 +92,34 @@ def pixel_to_grid(mouse_x, mouse_y):
         grid_y = mouse_y / GAP_SIZE
     else:
         grid_y = None
+    if (grid_x != None) and (grid_y != None):
+        grid_x = int(grid_x)
+        grid_y = int(grid_y)
+        print("grid_x =", grid_x, "grid_y =", grid_y)
     return grid_x, grid_y
+
+def grid_to_pixel(grid_x, grid_y):
+    pixel_x = (grid_x * GAP_SIZE)
+    pixel_y = (grid_y * GAP_SIZE)
+    return pixel_x, pixel_y
+
+def is_click_valid(grid_x, grid_y):
+    array_x = grid_x - 1
+    array_y = grid_y - 1
+    print("array_x =", array_x, "array_y =", array_y)
+    valid = grid_array[array_y][array_x]
+    print(valid)
+    if grid_array[array_y][array_x] == True:
+        grid_array[array_y][array_x] = False
+    return valid
+
+def draw_dot(grid_x, grid_y, player):
+    if player == PLAYER_ONE:
+        player_colour = PLAYER_ONE_COLOUR
+    else:
+        player_colour = PLAYER_TWO_COLOUR
+    pixel_x, pixel_y = grid_to_pixel(grid_x, grid_y)
+    pygame.draw.circle(DISPLAYSURF, player_colour, (pixel_x, pixel_y), 4, 0)
 
 if __name__ == '__main__':
     main()
